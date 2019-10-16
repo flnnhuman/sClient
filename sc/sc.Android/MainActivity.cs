778527830b9bc3d920fd18723d6ddaefe305491e
Vitaliy Chumakov <vitalyachuma@rambler.ru>
2019-10-16 22:20:08 +0300

    Forgot to commit

--- sc/sc.Android/MainActivity.cs
+++ sc/sc.Android/MainActivity.cs
@@ -1,26 +1,21 @@
-﻿using System;
-using Acr.UserDialogs;
+﻿using Acr.UserDialogs;
 using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
 
-namespace sc.Android
-{
-    [Activity(Label = "sc", Theme = "@style/MainTheme", MainLauncher = true,
-        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-    {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
-            UserDialogs.Init(this);
-            base.OnCreate(savedInstanceState);
-            Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
-        }
-    }
+namespace sc.Android {
+	[Activity(Label = "sc", Theme = "@style/MainTheme", MainLauncher = true,
+		ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : FormsAppCompatActivity {
+		protected override void OnCreate(Bundle savedInstanceState) {
+			TabLayoutResource = Resource.Layout.Tabbar;
+			ToolbarResource = Resource.Layout.Toolbar;
+			UserDialogs.Init(this);
+			base.OnCreate(savedInstanceState);
+			Forms.Init(this, savedInstanceState);
+			LoadApplication(new App());
+		}
+	}
 }
