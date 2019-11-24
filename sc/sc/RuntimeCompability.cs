fd505c6e69e7905591115088d3f4a6fdd0ad50fe
flnnhuman <peseckiyvlad@gmail.com>
2019-11-25 00:08:09 +0300

    RuntimeCompability

---
+++ sc/sc/RuntimeCompability.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Hosting;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using System.Threading;
+namespace sc
+{
+public static class RuntimeCompatibility {
+	public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> kv, out TKey key, out TValue value) {
+			key = kv.Key;
+			value = kv.Value;
+		}
+}
+}
